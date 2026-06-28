36cdd65ac0cfae147385b775168ae59b62ad459a
Shubham Mittal <shubhammittal2001@zohomail.in>
2026-06-28 12:40:20 +0530

    fix: load .env from repo root in server index.ts
    Signed-off-by: Shubham Mittal <shubhammittal2001@zohomail.in>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+dotenv.config({ path: resolve(dirname(fileURLToPath(import.meta.url)), '../../.env') })
 import Fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
 import fastifyCors from '@fastify/cors'
